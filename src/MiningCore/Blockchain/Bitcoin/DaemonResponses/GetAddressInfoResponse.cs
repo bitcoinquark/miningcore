bcd6e2b1cd0aca69c79f9d950344392b8bb76996
Mike <mike@bitcoinquark.org>
2018-04-03 15:18:57 +0800

    Bitcoin Quark v0.16.1

---
+++ src/MiningCore/Blockchain/Bitcoin/DaemonResponses/GetAddressInfoResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiningCore.Blockchain.Bitcoin.DaemonResponses
+{
+    public class GetAddressInfoResponse
+    {
+        public string Address { get; set; }
+
+        public string ScriptPubKey { get; set; }
+
+        public bool IsMine { get; set; }
+
+        public bool IsWatchOnly { get; set; }
+
+        public bool IsScript { get; set; }
+
+        public bool IsWitness { get; set; }
+
+        public string Script { get; set; }
+
+        public string Hex { get; set; }
+
+        public string PubKey { get; set; }
+
+        public string Account { get; set; }
+    }
+}
